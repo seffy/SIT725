abe4fe086db9fdff83d5d06aef1f5ff5ef78b149
seffy <seffy.sabana@gmail.com>
2024-11-23 00:48:14 +1100

    Task 3.2P

---
+++ Task 3.2P/public/js/script.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    $('.modal').modal();
+	$('#button2').click(function() {
+        $('#button1').css({
+          'background-color': 'red',
+          'color': 'white'
+        });
+      
+      });
+
+    $('#button3').click(function() {
+        $('#button1').css('background-color', 'blue');
+      });
+
+    $('#button4').click(function() {
+        $('#button1').css('background-color', 'green'); 
+      });
+	
+	
+	
+	
+	
+	
+	
+	
+	
+  });
+
+
