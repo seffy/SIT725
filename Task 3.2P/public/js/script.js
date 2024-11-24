12532fc18c60cc44cced4139467247790d3b5efd
seffy <seffy.sabana@gmail.com>
2024-11-25 05:51:43 +1100

    Task 32.P update

--- Task 3.2P/public/js/script.js
+++ Task 3.2P/public/js/script.js
@@ -1,40 +1,34 @@
 
 $(document).ready(function(){
+
+  // Modal Initialization
     $('.modal').modal();
+
+// Adding Buttons to Change Another Button's Color Using jQuery
 	$('#button2').click(function() {
-        $('#button1').css({
+        $('#button1x, #button1').css({
           'background-color': 'red',
           'color': 'white'
         });
-      
       });
 
     $('#button3').click(function() {
-      $('#button1').css({
-        'background-color': 'orange',
-        'color': 'white'
-      });
+        $('#button1x, #button1').css({
+          'background-color': 'orange',
+          'color': 'white'
+        });
       });
 
     $('#button4').click(function() {
-      $('#button1').css({
-        'background-color': 'green',
-        'color': 'white'
+        $('#button1x, #button1').css({
+          'background-color': 'green',
+          'color': 'white'
+        }); 
       });
-      });
-
 
       $('#vgitbtn').click(function() {
         window.open('https://github.com/seffy/SIT725/tree/main/Task%203.2P', '_blank');
       });
-	
-	
-	
-	
-	
-	
-	
-	
 	
   });
 
