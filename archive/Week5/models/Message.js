f310f7989ebc6630678d055259f3f8b4dc9c6662
Joseph Jhon Sabana <seffy.sabana@gmail.com>
2024-12-13 17:52:30 +1100

    Week 5

--- archive/Week5/models/Message.js
+++
@@ -1,9 +0,0 @@
-// models/Message.js
-const mongoose = require('mongoose');
-
-const messageSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }, // Automatically set the timestamp
-});
-
-module.exports = mongoose.model('Message', messageSchema);
--- archive/sit725-2024-t3-prac7/test/test.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const expect = chai.expect;
-
-describe('Array', function () {
-    it('should start empty', function () {
-        const arr = [];
-        expect(arr).to.be.an('array').that.is.empty;
-    });
-});
